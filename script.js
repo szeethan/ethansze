93aa0b046221cdfeef7850278e14d5e649eda0f7
Ethan Sze <esze@sze-2.mynetworksettings.com>
2024-03-13 19:13:44 -0400

    some work

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-// Simple smooth scroll function
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+// JavaScript for interactive elements
+document.querySelectorAll('details').forEach((detail) => {
+    const summary = detail.querySelector('summary');
+    const content = detail.querySelector('div');
+  
+    summary.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (detail.hasAttribute('open')) {
+        // Transition to closed state
+        content.style.height = `${content.scrollHeight}px`;
+        requestAnimationFrame(() => {
+          content.style.height = '0px';
+        });
+        detail.removeAttribute('open');
+      } else {
+        // Transition to open state
+        detail.setAttribute('open', 'open');
+        content.style.height = `${content.scrollHeight}px`;
+      }
+    });
+  
+    detail.addEventListener('toggle', () => {
+      if (detail.open) {
+        content.style.height = `${content.scrollHeight}px`;
+      } else {
+        content.style.height = '0px';
+      }
     });
   });
-});
